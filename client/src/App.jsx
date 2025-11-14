814d0aecb32f5b4459fdd6558cd2e8637470e003
Prashant-SG14 <prashant.a1206@gmail.com>
2025-11-14 16:26:49 +0530

    feat: Add routing for repo insights page
    Integrated `react-router-dom` to enable navigation and created a dedicated page for repository insights.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,52 @@
 import { useState } from "react";
 
-import ThemeToggle from "./components/ThemeToggle";
+import { Routes, Route, useParams } from "react-router-dom";
 
 import "./App.css";
 import Topbar from "./components/Topbar";
 import Footer from "./components/Footer";
 import DomainSelector from "./components/DomainSelector";
 
+function HomePage({ topics, setTopics }) {
+  return (
+    <main className="mx-auto max-w-5xl flex w-full flex-col gap-10 px-4 py-12 ">
+      <DomainSelector setTopics={setTopics} />
+      {/*Gotta add Card Components here*/}
+    </main>
+  );
+}
+
+function Insights() {
+
+  const { owner, repo } = useParams();
+
+  return (
+    <main className="mx-auto max-w-5xl flex w-full flex-col gap-10 px-4 py-12 ">
+
+      <h1 className="text-2xl font-semibold tracking-tight text-[var(--text-strong)] sm:text-3xl">
+        Insights for: {owner}/{repo}
+      </h1>
+
+      {/* Add commit chart n other stuff  */}
+    </main>
+  );
+}
+
 function App() {
-  const [count, setCount] = useState(0);
   const [topics, setTopics] = useState([]);
 
   return (
     <div className="flex min-h-screen flex-col bg-[var(--bg-primary)] text-[var(--text-primary)] antialiased">
       <Topbar />
-      <main className="mx-auto max-w-5xl flex w-full flex flex-col gap-10 px-4 py-12 ">
-        <DomainSelector setTopics={setTopics} />
-      </main>
+
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage topics={topics} setTopics={setTopics} />}
+        />
+
+        <Route path="/repo/:owner/:repo" element={<Insights />} />
+      </Routes>
 
       <Footer />
     </div>
